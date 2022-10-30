a676f476cfb878e12d323db746271d28a54c8d67
Jeck-Lee <jecklee02@gmail.com>
2022-10-30 12:01:26 +0900

    누락 코드 추가

--- MoodDiary/Extension/Date+Ext.swift
+++ MoodDiary/Extension/Date+Ext.swift
@@ -23,4 +23,33 @@ extension Date {
 		let days2 = calendar.component(component, from: date)
 		return days1 - days2
 	}
+	
+	// MARK: 날짜 생성
+	/// Create a date from specified parameters
+	///
+	/// - Parameters:
+	///   - year: The desired year
+	///   - month: The desired month
+	///   - day: The desired day
+	/// - Returns: A `Date` object
+	static func from(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil) -> Date? {
+		let calendar = Calendar(identifier: .gregorian)
+		var dateComponents = DateComponents()
+		if let year = year {
+			dateComponents.year	= year
+		}
+		if let month = month {
+			dateComponents.month = month
+		}
+		if let day = day {
+			dateComponents.day	= day
+		}
+		if let hour = hour {
+			dateComponents.hour	= hour
+		}
+		if let minute = minute {
+			dateComponents.minute = minute
+		}
+		return calendar.date(from: dateComponents) ?? nil
+	}
 }
